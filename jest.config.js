95e22fef29ea56ae5170c4302e4ffad687634d28
optydeviocourses <optydev.io.courses@gmail.com>
2021-04-20 18:00:47 -0300

    Ajustes

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules', '/.next/'],
+  testPathIgnorePatterns: ['/node_modules', '/.next/', '/.vscode'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
